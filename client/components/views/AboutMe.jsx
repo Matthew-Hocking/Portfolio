508902c7f8168e065ce564ffc7a846052b2b3df7
matthew-hocking <matthocking2439@gmail.com>
2022-01-28 15:04:45 +1300

    typography adjusted

--- client/components/views/AboutMe.jsx
+++ client/components/views/AboutMe.jsx
@@ -3,6 +3,11 @@ import React from 'react'
 const AboutMe = () => {
   return (
     <div className="container">
+      <div className='profile-container'>
+        <img id="profile-image" src="images/profile.png" alt='profile image of Matthew Hocking' />
+      </div>
+        <h1 id="heading">Howdy 🤠, I'm a front-end developer from New Zealand 🥝.</h1>
+        <h1 id='heading'>Ever since I was a toddler I've enjoyed pulling things apart and putting them back together again.</h1>
     </div>
   )
 }
